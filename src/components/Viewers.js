6ae31edda5d59187bd9f2dcd0fba096687081f77
John Doe <johndoe@example.com>
2021-04-27 01:36:45 +0530

    some changes

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,95 +1,96 @@
 import styled from 'styled-components'
 
-const Viewers = (props) =>{
+const Viewers = (props) => {
     return (
         <Container>
             <Wrap>
-                <img src='/images/viewers-disney.png' alt=''/>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/FFA0BEBAC1406D88929497501C84019EBBA1B018D3F7C4C3C829F1810A24AD6E/scale?width=600&aspectRatio=1.78&format=png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
-        </video>
+                    <source src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/01/1564674844-disney.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-marvel.png' alt=''/>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F4E1A299763030A0A8527227AD2812C049CE3E02822F7EDEFCFA1CFB703DDA5/scale?width=600&aspectRatio=1.78&format=png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
-        </video>
+                    <source src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/01/1564676714-pixar.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-national.png' alt=''/>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C90088DCAB7EA558159C0A79E4839D46B5302B5521BAB1F76D2E807D9E2C6D9A/scale?width=600&aspectRatio=1.78&format=png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
-        </video>
+                    <source src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/01/1564676115-marvel.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-pixar.png' alt=''/>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5A9416D67DC9595496B2666087596EE64DE379272051BB854157C0D938BE2C26/scale?width=600&aspectRatio=1.78&format=png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
-        </video>
+                    <source src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2020/12/17/1608229455-star-wars.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-starwars.png' alt=''/>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/2EF24AA0A1E648E6D1A3B26491F516632137ED87AB22969D153316F8BD670FB5/scale?width=600&aspectRatio=1.78&format=png" alt="" />
                 <video autoPlay={true} loop={true} playsInline={true}>
-          <source
-            src="/videos/1564676296-national-geographic.mp4"
-            type="video/mp4"
-          />
-        </video>
+                    <source src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/01/1564676296-national-geographic.mp4" type="video/mp4" />
+                </video>
             </Wrap>
         </Container>
     )
 }
 
 const Container = styled.div`
-  margin-top: 30px;
-  padding: 30px 0px 26px;
-  display: grid;
-  grid-gap: 25px;
-  gap: 25px;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(1, minmax(0, 1fr));
-  }
+    margin-top: 30px;
+    padding: 30px 0px 26px;
+    display: grid;
+    grid-gap: 25px;
+    gap: 25px;
+    grid-template-columns: repeat(5,minmax(0,1fr));
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(1,minmax(0,1fr));
+    }
 `;
 
-const Wrap= styled.div`
-padding-top: 56.25%;
-border-radius: 10px;
-box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-  rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-cursor: pointer;
-overflow: hidden;
-position: relative;
-transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-border: 3px solid rgba(249, 249, 249, 0.1);
-img {
-  inset: 0px;
-  display: block;
-  height: 100%;
-  object-fit: cover;
-  opacity: 1;
-  position: absolute;
-  transition: opacity 500ms ease-in-out 0s;
-  width: 100%;
-  z-index: 1;
-  top: 0;
-}
-video {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0px;
-  opacity: 0;
-  z-index: 0;
-}
-&:hover {
-  box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-    rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-  transform: scale(1.05);
-  border-color: rgba(249, 249, 249, 0.8);
-  video {
-    opacity: 1;
-  }
-}
+const Wrap = styled.div`
+    padding-top: 56.25%;
+    border-radius: 10px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    overflow: hidden;
+    position: relative;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+
+    img {
+        inset: 0px;
+        display: block;
+        height: 100%;
+        object-fit: cover;
+        opacity: 1;
+        position: absolute;
+        transition: opacity 500ms ease-in-out 0s;
+        width: 100%;
+        z-index: 1;
+        top: 0;
+    }
+    
+    video {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0px;
+        opacity: 0;
+        z-index: 0;
+    }
+
+    &:hover {
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8);
+
+        video {
+            opacity: 1;
+        }
+    }
 `;
+
 export default Viewers
